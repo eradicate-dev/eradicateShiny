9dd613a6b7d8e94e3f1db6cbba71ec7d03479ee2
Michael P Scroggie (DELWP) <Michael.Scroggie@delwp.vic.gov.au>
2020-03-20 10:21:33 +1100

    fixed export of run_eradication_app function

--- R/run_eradication_app.R
+++ R/run_eradication_app.R
@@ -1,5 +1,5 @@
 #' @export
-run_assessment_app <- function() {
+run_eradication_app <- function() {
 	appDir <- system.file("shiny-examples", "eradication_app", package = "eradicateShiny")
 	if (appDir == "") {
 		stop("Could not find example directory. Try re-installing `eradicateShiny`.", call. = FALSE)

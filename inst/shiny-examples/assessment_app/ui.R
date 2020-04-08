07cfff9d37272bba947c22eab18baa9e01ccfd44
Michael P Scroggie (DELWP) <Michael.Scroggie@delwp.vic.gov.au>
2020-04-08 12:58:48 +1000

    Made minor changes to assessment ui to make consistent with eradication ui

--- inst/shiny-examples/assessment_app/ui.R
+++ inst/shiny-examples/assessment_app/ui.R
@@ -21,7 +21,7 @@ ui<-fluidPage(
 	titlePanel("Pre-implementation Monitoring"),
 	sidebarLayout(
 		sidebarPanel(#INPUT THE REGION, detector and detections FILEs ---------------------------------------------------
-								 fileInput(inputId="boundary", label="Region Boundary (.shp)",
+								 fileInput(inputId="boundary", label="Region boundary (.shp)",
 								 					multiple=TRUE,  accept=c('.shp','.dbf','.sbn','.sbx','.shx','.prj')),
 								 fileInput(inputId="habitat_rasters", label="Habitat raster (.tif, .asc)", accept=c(".tif", ".asc")),
 								 fileInput(inputId="detectors", label="Detector locations (.csv)", accept=c("text/csv", ".csv")),
@@ -30,12 +30,12 @@ ui<-fluidPage(
 								 fluidRow(
 								 column(3, actionButton("Plot_design", "Plot map")),
 								 column(3, numericInput("habitat_radius", "Raster sampling radius", min=0, max=NA, value=500, step=50)),
-								 column(5, sliderInput("Habitat_opacity", "Raster opacity", min=0, max=1, value=0.2, step=0.1))
+								 column(5, sliderInput("Habitat_opacity", "Raster opacity", min=0, max=1, value=0.2, step=0.2))
 								 ),
 								 hr(),
 								 #SELECT APPROPRIATE MODEL --------------------------------------------------------------------------
 								 fluidRow(
-								 column(3, radioButtons(inputId="Model", label="Model to Fit",
+								 column(3, radioButtons(inputId="Model", label="Select model",
 								 						 choices=list("Occupancy"="Occ",
 								 						 						 "Royle-Nichols"="RN",
 								 						 	            "N-mixture"="Nmix"
@@ -43,7 +43,7 @@ ui<-fluidPage(
 								 conditionalPanel("input.Model!='Occ'",
 								 column(2, numericInput(inputId="K", label="K", min=1, max=NA, value=50, step=1),
 								           checkboxInput("EstDens", "Estimate Density", value=FALSE))),
-								 column(2, actionButton(inputId="Run_model", label="Fit Model") ) ,width=4, fluid=TRUE)
+								 column(2, actionButton(inputId="Run_model", label="Fit model") ) ,width=4, fluid=TRUE)
 								 ),
 
 		mainPanel(
@@ -52,7 +52,7 @@ ui<-fluidPage(
 									tabPanel(title="Map",	                value="panel1",
 													    leafletOutput(outputId = "map", height=700)),
 									#Fitted models tab
-									tabPanel(title="Fitted Model",        value="panel2",
+									tabPanel(title="Fitted model",        value="panel2",
 													    fluidRow( tableOutput(outputId="parameter_table") %>% withSpinner(type=4)),
 													    fluidRow( tableOutput(outputId="abundance_table") %>% withSpinner(type=4)),
 													 ))

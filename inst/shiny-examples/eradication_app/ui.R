5abe9094da5056b7d0ffdae3ef3af0f7d0b6ef5e
Michael Scroggie <scrogster@gmail.com>
2020-10-26 18:18:09 +1100

    changes to UI to hide/show tools relevant to each model type

--- inst/shiny-examples/eradication_app/ui.R
+++ inst/shiny-examples/eradication_app/ui.R
@@ -17,17 +17,18 @@ ui<-fluidPage(
 	sidebarLayout(
 		sidebarPanel(#INPUT THE REGION, detector and detections FILEs ---------------------------------------------------
 								 fluidRow(
-								 	column(5,
+								 	column(6,
 								 				 radioButtons(inputId="Model", label="Select model",
-								 				 						 choices=list("remCE"="remCE",
-								 				 						 						 "remGP"="remGP",
+								 				 						 choices=list("remCE (aspatial)"="remCE",
+								 				 						 						 "remGP (aspatial)"="remGP",
 								 				 						 						 "remGR"="remGR",
 								 				 						 						 "remGRM"="remGRM",
 								 				 						 						 "remMN"="remMN",
-								 				 						 						 "remMN"="remMNO"
-								 				 						 ), selected="remGP")),
+								 				 						 						 "remMN"="remMNO",
+								 				 						 						 "occuMS"="occuMS"
+								 				 						 ), selected="remCE")),
 								 	#conditionally take input for parameter K, depending on model type.
-								 	conditionalPanel("input.Model!='CE'&&input.Model!='remMN'",
+								 	conditionalPanel("input.Model!='remCE'&&input.Model!='remMN'&&input.Model!='occuMS'",
 								 									 column(2, numericInput(inputId="K", label="K", min=1, max=NA, value=50, step=1)
 								 									 ))),
 								 hr(),
@@ -36,11 +37,12 @@ ui<-fluidPage(
 								 					multiple=TRUE,  accept=c('.shp','.dbf','.sbn','.sbx','.shx','.prj')),
 								 tipify(fileInput(inputId="habitat_rasters", label="Habitat raster (.tif)",
 								 								 multiple=TRUE, accept=c(".tif")),
-								 			 "Raster files (.tif or .asc) of habitat covariates in the same projection as the boundary.
+								 			 "Raster files (.tif) of habitat covariates in the same projection as the boundary.
 								 			    All rasters must have the same extent and resolution"),
 								 fileInput(inputId="traps", label="Trap locations (.csv)", accept=c("text/csv", ".csv")),
 								 fileInput(inputId="removals", label="Removal histories (.csv)", accept=c("text/csv", ".csv")),
-								 fileInput(inputId="detections", label="Detection histories (.csv)", accept=c("text/csv", ".csv")),
+								 conditionalPanel("input.Model=='remGRM'",
+								 fileInput(inputId="detections", label="Detection histories (.csv)", accept=c("text/csv", ".csv"))),
 								 fluidRow(
 								 	column(4,
 								 numericInput("nights", "Nights per primary session", min=0, max=NA, value=10, step=1)))
@@ -48,18 +50,19 @@ ui<-fluidPage(
 								 hr(),
 								 fluidRow(
 								 	column(3, actionButton("Plot_design", "Plot map"), actionButton("Plot_removal", "Plot removals")),
+								 	conditionalPanel("input.Model!='remCE'&&input.Model!='remGP'",
 								 	column(4, numericInput("habitat_radius", "Raster sampling radius", min=0, max=NA, value=500, step=50)),
-								 	column(5, sliderInput("Habitat_opacity", "Raster opacity", min=0, max=1, value=0.2, step=0.2))
+								 	column(5, sliderInput("Habitat_opacity", "Raster opacity", min=0, max=1, value=0.2, step=0.2)))
 								 ),
 								 hr(),
 								 #SELECT APPROPRIATE MODEL --------------------------------------------------------------------------
 								 fluidRow(
-								 	column(6,
+								 	column(6,conditionalPanel("input.Model!='remCE'&&input.Model!='remGP'",
 								 				 tipify(checkboxGroupInput(inputId="state_formula", label="habitat covariates",
 								 				 													choices=NULL,
 								 				 													selected=NULL
 								 				 ),"Select habitat variables for inclusion in abundance model.
-								 			 			               If none selected an intercept-only model will be fitted")
+								 			 			               If none selected an intercept-only model will be fitted"))
 
 								 	),
 								 column(2, actionButton(inputId="Run_model", label="Fit model")) ,width=4, fluid=TRUE)),
